7ad5fa726b2efd7b7ad4a68121421113c2937789
Ocean <hophuong.ocean@gmail.com>
2024-03-14 17:01:28 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == '__main__':
+
+    linked_list = LinkedList()
