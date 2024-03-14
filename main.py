2292b27af9c0554174460a4e46e907bc25377c72
Ocean <hophuong.ocean@gmail.com>
2024-03-14 17:11:25 +0000

    added values for initialisation of linked list

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ class LinkedList:
 
 
 if __name__ == '__main__':
-
+    # initialise linked list with some values
     linked_list = LinkedList()
+
+    # Assign item values
+    linked_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    # Connect nodes
+    linked_list.head.next = second
+    second.next = third
